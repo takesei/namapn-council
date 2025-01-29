8bbb39e7c1dda7f1f8805cf1e29c1244c2a94f4c
taka-sho <takasho530000@gmail.com>
2025-01-30 01:31:57 +0900

    feat: can get all of files in target folder

--- pipelines/get_spread_sheet_urls/index.ts
+++ pipelines/get_spread_sheet_urls/index.ts
@@ -1,27 +1,33 @@
 import * as ff from "@google-cloud/functions-framework";
 import { drive } from "@googleapis/drive";
-import { google } from "googleapis";
 import type { HttpFunction } from "@google-cloud/functions-framework";
 
 export const getSpreadSheetUrls: HttpFunction =  async (req: ff.Request, res: ff.Response) => {
-  const credentials = await google.auth.getCredentials();
+  const DRIVE_API_KEY = process.env.DRIVE_API_KEY;
   const driveClient = drive({
     version: "v3",
-    auth: JSON.stringify(credentials),
+    auth: DRIVE_API_KEY,
   });
+
+  const folderId = req.query['folder_id'];
+  if (!folderId) {
+    res.send("query parameterにfolder_idを指定してください");
+    return;
+  }
+
+  console.log(`取得対象のフォルダID: ${folderId}`);
 
   try {
     const fileResponse = await driveClient.files.list({
-      // TODO: reqからdriveのフォルダIDを取得するようにする
-      q: "1-jfXdav0UHKDo28-fYthHbEx674kbsG3",
+      q: `'${folderId}' in parents and trashed = false`,
     });
     const files = fileResponse.data.files;
     if (!files) {
-      res.send("レスポンスがありません");
+      res.json([]);
       return;
     }
     if (files.length === 0) {
-      res.send("ファイルがありません");
+      res.json([]);
     }
 
     const spreadSheetUrls = files.reduce((acc, curr) => {
@@ -35,8 +41,8 @@ export const getSpreadSheetUrls: HttpFunction =  async (req: ff.Request, res: ff
       }
     }, {});
 
-    res.send(JSON.stringify(spreadSheetUrls));
+    res.status(200).json(spreadSheetUrls);
   } catch(e: unknown) {
-    res.send(`エラーが起こりました．error: ${e}`);
+    res.status(500).send(`エラーが起こりました．error: ${e}`);
   }
 };
