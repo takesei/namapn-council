d2ef207f011b2359645f0bb2da044546e8fd5dfc
taka-sho <takasho530000@gmail.com>
2025-02-01 19:15:26 +0900

    feat: can update sheet correctly

--- pipelines/update_spread_sheet/index.ts
+++ pipelines/update_spread_sheet/index.ts
@@ -1,39 +1,59 @@
-import * as ff from "@google-cloud/functions-framework";
-import { drive } from "@googleapis/drive";
+import type * as ff from "@google-cloud/functions-framework";
+import { auth, drive } from "@googleapis/drive";
 import type { HttpFunction } from "@google-cloud/functions-framework";
+import { build as buildXlsx } from "node-xlsx";
+import { parse as parseCsv } from "csv-parse/sync";
+import { Readable } from "node:stream";
 
-export const updateSpreadSheet: HttpFunction =  async (req: ff.Request, res: ff.Response) => {
-  const DRIVE_API_KEY = process.env.DRIVE_API_KEY;
-  const driveClient = drive({
-    version: "v3",
-    auth: DRIVE_API_KEY,
-  });
+export const updateSpreadSheet: HttpFunction = async (
+	req: ff.Request,
+	res: ff.Response,
+) => {
+	const googleAuth = new auth.GoogleAuth({
+		scopes: ["https://www.googleapis.com/auth/drive"],
+	});
+	const driveClient = drive({
+		version: "v3",
+		auth: googleAuth,
+	});
 
-  const fileId = req.body['file_id'];
-  if (!fileId) {
-    res.send("bodyにfile_idを指定してください");
-    return;
-  }
+	const fileId = req.body.file_id;
+	if (!fileId) {
+		res.send("bodyにfile_idを指定してください");
+		return;
+	}
 
-  const fileContents = req.body['file_contents'];
-  if (!fileContents) {
-    res.send("bodyにfile_contentsを指定してください");
-    return;
-  }
+	const fileContents = req.body.file_contents;
+	if (!fileContents) {
+		res.send("bodyにfile_contentsを指定してください");
+		return;
+	}
 
-  console.log(`変更対象のファイルID: ${fileId}`);
+	console.log(`変更対象のファイルID: ${fileId}`);
 
-  try {
-    await driveClient.files.update({
-      fileId,
-      media: {
-        body: fileContents,
-        mimeType: "text/csv"
-      }
-    })
+	const parsedCsv = parseCsv(Buffer.from(fileContents)) as string[][];
 
-    res.status(200);
-  } catch(e: unknown) {
-    res.status(500).send(`エラーが起こりました．error: ${e}`);
-  }
+	const xlsxData = buildXlsx([{ data: parsedCsv, name: "", options: {} }]);
+	try {
+		await driveClient.files.update({
+			fileId,
+			supportsAllDrives: true,
+			requestBody: {
+				mimeType:
+					"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+			},
+			media: {
+				body: Readable.from(xlsxData),
+				mimeType:
+					"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+			},
+			uploadType:
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+		});
+
+		res.status(200).send();
+	} catch (e: unknown) {
+		console.log(e);
+		res.status(500).send(`エラーが起こりました．error: ${e}`);
+	}
 };
