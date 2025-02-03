d455cf9f1e2e66c9d64c26c3d395ee4e03656eed
takesei <sei06k14@gmail.com>
2025-02-04 04:30:01 +0900

    :sparkles: support jinja rendering

--- webcli/modules/top/contingency_plan.py
+++ webcli/modules/top/contingency_plan.py
@@ -1,112 +1,97 @@
 import streamlit as st
-from typing import Any
-import pandas as pd
-from io import StringIO
+from libs.actions import run_action
 
-from vega_datasets import data
+
+@st.dialog("セッションを開始しますか?")
+def start_session():
+    c1, c2 = st.columns(2)
+    res = None
+    with c1:
+        if st.button("はい"):
+            res = True
+    with c2:
+        if st.button("いいえ"):
+            res = False
+
+    if res is None:
+        pass
+    elif res:
+        with st.status("情報取得中"):
+            st.write("被害予測取得中")
+            impact = st.session_state.db.get("impact")
+            st.write("対策シナリオセッション準備中")
+            prompt = f"""
+            イベントの詳細: {st.session_state.agent["event"]}
+            イベントの被害: {impact}
+            ユーザーのinput: イベントが発生しました, 次に何をしたらいいか教えてください
+            """
+            response = st.session_state.agent["model"].send_message(prompt)
+            st.write("情報登録中")
+            st.session_state.agent["chat_history"].append(
+                {"role": "assistant", "content": response["msg"], "fig": None}
+            )
+            st.session_state.agent["last_response"] = response["msg"]
+            st.session_state.agent["strategy"] = response["strategy"]
+            st.write("完了")
+            st.session_state.agent["status"] = "active"
+            st.rerun()
+    else:
+        st.info("待機します, ×ボタンから抜けてください")
+
+
+if st.session_state.agent["event"] is None:
+    st.info("重要なイベントはありません")
+    st.stop()
+if st.session_state.agent["status"] == "deactive":
+    "# イベント対応"
+    st.warning("**イベントを検知しました, 開始ボタンから状況を開始してください**")
+    if st.button("状況を開始"):
+        start_session()
+    st.stop()
 
 
 left, right = st.columns([0.6, 0.4], border=True, vertical_alignment="top")
-
-data = [
-    (data.barley(), dict(x="variety", y="yield", color="site", horizontal=True)),
-    (data.barley(), dict(x="year", y="yield", color="site", stack=False)),
-]
-
-
-@st.dialog("Change BI figure")
-def change_fig(tab: int, col: int):
-    f"change fig on {tab} {col}"
-
-
-def run_action(infos: list[dict[str, Any]]):
-    for info in infos:
-        print(f"got {info}")
-        if info["type"] == "plot":
-            input = info["input"]
-            if input["plot_type"] == "line":
-                st.line_chart(
-                    data=pd.read_json(StringIO(input["source"])),
-                    x=input["x_axis"],
-                    y=input["y_axis"],
-                )
-            elif input["plot_type"] == "bar":
-                st.bar_chart(
-                    data=pd.read_json(StringIO(input["source"])),
-                    x=input["x_axis"],
-                    y=input["y_axis"],
-                )
-            else:
-                return None
-
-
 with right:
     st.subheader(":material/support_agent: Planning Companion", divider="grey")
     chat_history = st.container(border=True, height=650)
     with chat_history:
-        # Display chat messages from history on app rerun
-        for message in st.session_state.messages:
+        for message in st.session_state.agent["chat_history"]:
             with st.chat_message(message["role"]):
                 message["content"]
                 if "fig" in message and message["fig"] is not None:
                     message["fig"]
 
     with st.container(border=False):
-        # Accept user input
         if prompt := st.chat_input("What is up?"):
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            # Display user message in chat message container
+            st.session_state.agent["chat_history"].append(
+                {"role": "user", "content": prompt, "fig": None}
+            )
             with chat_history:
                 with st.chat_message("user"):
                     st.markdown(prompt)
-
-            # Display assistant response in chat message container
-            with chat_history:
                 with st.chat_message("assistant"):
-                    resp = st.session_state.aiagent.send_message(
-                        prompt,
-                        st.session_state.strategy_scenario,
-                        st.session_state.agent_ai_last_msg,
-                    )
+                    resp = st.session_state.agent["model"].send_message(prompt)
                     response = st.write(resp["msg"])
-                    st.session_state.agent_ai_last_msg = response
-                    st.session_state.strategy_scenario = resp["strategy"]
                     fig = run_action(resp["actions"])
-
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": response})
-            st.session_state.messages.append(
-                {"role": "assistant", "content": response, "fig": fig}
+            st.session_state.agent["chat_history"].append(
+                {"role": "assistant", "content": resp["msg"], "fig": fig}
             )
 
 
 with left:
-    st.subheader(":material/checklist: _Action List_", divider="red")
-    with st.container(border=True, height=150):
-        st.markdown(
-            """
-         - [ ] Supplier 102の供給への影響を早急に確認してください。
-         - 供給不可の場合、下記順序で対応してください
-           - [ ] Supplier 103の追加供給の可能性確認
-           - [ ] Supplier 101の追加供給の可能性確認
-         """
-        )
-
-    st.subheader(":material/monitoring: _BI PickUps_", divider="blue")
-    tabs = st.tabs(["1. XXX", "2. XXX", "3. XXX", ":material/settings: configure"])
-    for tab_idx, tab in enumerate(tabs[:-1]):
-        with tab:
-            with st.container():
-                for col_idx, col in enumerate(st.columns(2)):
-                    with col:
-                        with st.container(height=400, border=True):
-                            val, kwarg = data[col_idx % 2]
-                            st.bar_chart(val, **kwarg)
-    with tabs[-1]:
-        if st.button(
-            "change figures",
-            icon=":material/settings:",
-            type="tertiary",
-        ):
-            change_fig(tab_idx, col_idx)
+    tabs = st.tabs(["1. イベント概要", "イベント詳細", "2. 対策1", "3. 対策2"])
+    with tabs[0]:
+        with st.container(height=700):
+            template = st.session_state.jinja.get_template("event.md")
+            rdr = template.render(st.session_state.agent["event"])
+            st.markdown(rdr)
+    with tabs[1]:
+        with st.container(height=700):
+            template = st.session_state.jinja.get_template("event.md")
+            rdr = template.render(st.session_state.agent["event"])
+            st.markdown(rdr)
+    with tabs[2]:
+        with st.container(height=700):
+            template = st.session_state.jinja.get_template("strategy.md")
+            rdr = template.render(st.session_state.agent["model"].strategy)
+            st.markdown(rdr)
