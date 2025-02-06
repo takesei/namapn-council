1fb61c1a3bca6ad5ce8abb439727433ec251340b
takesei <sei06k14@gmail.com>
2025-02-06 12:34:45 +0900

    :sparkles: cover forecasts

--- dbt/models/forecast/production_forecasts.sql
+++ dbt/models/forecast/production_forecasts.sql
@@ -1,7 +1,24 @@
 with
---- final
-final as (
+--- load
+forecast as (
   select * from {{ source("forecast", "production_forecasts") }}
+),
+time as(
+  select 
+    *
+  from {{ ref("times") }}
+),
+
+---final
+final as (
+  select
+    f.version,
+    t.time_id,
+    f.line_code,
+    f.item_code,
+    f.quantity
+  from forecast as f
+  left join time as t on f.year_code = t.year and f.month_code = t.month and f.date_code = t.date
 )
 
 select * from final
