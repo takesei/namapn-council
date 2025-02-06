fac2751334730d53953d27de40d72140857db4af
takesei <sei06k14@gmail.com>
2025-02-07 04:33:23 +0900

    :sparkles: add temporary tables

--- webcli/modules/planning/proration.py
+++ webcli/modules/planning/proration.py
@@ -2,16 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-target_file = "data/sales/dim/product.csv"
-
-if "supply_plan" not in st.session_state:
-    supply_plan = pd.read_csv(target_file, index_col="id")
-else:
-    supply_plan = st.session_state.supply_plan
-
-
-def run_simulator(path: str) -> pd.DataFrame:
-    st.session_state.supply_plan = pd.read_csv(path)
-
-
-st.data_editor(supply_plan, num_rows="dynamic")
+"# 調達計画"
+with st.spinner("loading"):
+    df = st.session_state.db.get("resource_forecasts")
+    df
--- webcli/modules/planning/supply.py
+++ webcli/modules/planning/supply.py
@@ -1,37 +1,7 @@
 import streamlit as st
-import pandas as pd
-import time
 
 
-def optimize():
-    with st.spinner("optimizing..."):
-        time.sleep(1)
-    st.success("Done!")
-    df = st.session_state.sp
-    psi = st.session_state.psi
-    st.session_state.sp.prod_cost = df.jan_code * (4 - (df.annex_id % 4)) * psi.P * 0.8
-    st.session_state.sp.store_cost = psi.I * (df.annex_id % 4) * 0.7
-    st.session_state.sp.transportatoin_cost = psi.P * (df.annex_id % 4) * 1.5
-
-
-if "sp" not in st.session_state:
-    df = st.session_state.dp.loc[
-        :,
-        [
-            "jan_code",
-            "jan_name",
-            "hq_code",
-            "hq_name",
-            "month",
-            "week_of_month",
-            "quantity",
-        ],
-    ]
-    df = pd.merge(df, st.session_state.annex, on="hq_code")
-    df.quantity *= df.ratio
-    df = df.drop(columns=["hq_code", "hq_name", "ratio"])
-    df = df.assign(prod_cost=None, store_cost=None, transportatoin_cost=None)
-    st.session_state.sp = df
-
-st.session_state.sp = st.data_editor(st.session_state.sp)
-st.button("optimize", on_click=optimize)
+"# 供給計画"
+with st.spinner("loading"):
+    df = st.session_state.db.get("production_forecasts")
+    df
