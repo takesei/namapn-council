5342ae7644cd1001bc33132026fea08d49d666d0
takesei <sei06k14@gmail.com>
2025-02-06 13:38:32 +0900

    :wrench: change menu contents

---
+++ webcli/modules/data/overview.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+"""
+# データ分析/設定ページ
+
+## About
+ここでは, データの属性情報の設定や, 分析を実施することができます
+
+## Reference
+- schema: https://app.diagrams.net
+- database: https://console.cloud.google.com/bigquery
+"""
