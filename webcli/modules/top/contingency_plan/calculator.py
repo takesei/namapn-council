6603715d4e879a1f43ba108442ad42888e99f7ff
takesei <sei06k14@gmail.com>
2025-02-17 13:05:21 +0900

    :recycle: update provisioning

--- webcli/modules/top/contingency_plan/calculator.py
+++ webcli/modules/top/contingency_plan/calculator.py
@@ -2,17 +2,34 @@ import streamlit as st
 from modules.top.contingency_plan.companion import Message
 
 
-def provision_data():
+def provision_data(current_version_code: str):
     with st.status("情報取得中"):
         st.write("被害予測取得中")
 
         st.write("対策シナリオセッション準備中")
+        event = st.session_state.event
+        dhub = st.session_state.datahub
+        target_version = [c.version for c in event.event_cases]
+
+        event_effect = {tv: {} for tv in target_version}
+        for name, dcont in dhub.items():
+            for tv in target_version:
+                try:
+                    df_diff = dcont.diff([current_version_code, tv])
+                    event_effect[tv][name] = df_diff
+                except Exception as e:
+                    print(e)
+
+        prompt = ""
+        for k, v in event_effect.items():
+            prompt += f"-- {k}\n"
+            for table, diff in v.items():
+                prompt += f"---- {table}\n  {diff.to_json()}\n"
+
         prompt = f"""
-        [イベントシナリオ]: {st.session_state.agent["event"]}
+        [イベントシナリオ]: {st.session_state.event}
         eventの影響
-        1. sales: {st.session_state["diff_sales_forecast"]}
-        2. resource: {st.session_state["diff_resource_forecast"]}
-        3. routing: {st.session_state["diff_routing_forecast"]}
+        {prompt}
         ユーザーのinput: イベントが発生しました, 3行程度で概要を説明してください.
         また今は何をしたらいいでしょうか?
         """
@@ -21,7 +38,7 @@ def provision_data():
             Message(
                 actor="assistant",
                 message=response.message,
-                attachments=response.actions,
+                attachments=response.attachments,
             )
         )
         st.session_state.agent["status"] = "active"
--- webcli/modules/top/contingency_plan/main.py
+++ webcli/modules/top/contingency_plan/main.py
@@ -10,23 +10,23 @@ def start_session():
     c1, c2 = st.columns(2)
     with c1:
         if st.button("はい"):
-            provision_data()
+            provision_data("V001")
     with c2:
         if st.button("いいえ"):
             st.info("待機します, ×ボタンから抜けてください")
 
 
 # UI
-if st.session_state.agent["event"] is None:
+if st.session_state.event is None:
     st.info("重要なイベントはありません")
     st.stop()
 
-# if st.session_state.agent["status"] == "deactive":
-#     "# イベント対応"
-#     st.warning("**イベントを検知しました, 開始ボタンから状況を開始してください**")
-#     if st.button("状況を開始"):
-#         start_session()
-#     st.stop()
+if st.session_state.agent["status"] == "deactive":
+    "# イベント対応"
+    st.warning("**イベントを検知しました, 開始ボタンから状況を開始してください**")
+    if st.button("状況を開始"):
+        start_session()
+    st.stop()
 
 
 left, right = st.columns([0.6, 0.4], vertical_alignment="top")
@@ -40,6 +40,6 @@ with left:
     with st.container(border=True, height=800):
         jinja_env = st.session_state.jinja
         strategy_scenario = st.session_state.agent["model"].strategy
-        event_scenario = st.session_state.agent["event"]
+        event_scenario = st.session_state.event
         datahub = st.session_state.datahub
         render_report(jinja_env, event_scenario, strategy_scenario, datahub)
